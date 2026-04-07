642c3786e9da375ff70d25fda48a198a9acad002
AadithyaAle <aleaditya2008@gmail.com>
2026-04-08 01:58:08 +0530

    fix: fixed server file

--- server/__init__.py
+++ server/__init__.py
@@ -1,11 +1,10 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
-
 """Sst Hackathon Env environment server components."""
 
-from .SST_hackathon_env_environment import SstHackathonEnvironment
+# Try absolute import first (for Docker/Production)
+# Fall back to relative import (for Local Testing)
+try:
+    from server.SST_hackathon_env_environment import SstHackathonEnvironment
+except ImportError:
+    from .SST_hackathon_env_environment import SstHackathonEnvironment
 
 __all__ = ["SstHackathonEnvironment"]
